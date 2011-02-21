2d08c122fbea93506c81a3d90bad93bf148ed307
jaymz campbell <jaymzcd@googlemail.com>
2011-02-21 21:34:00 +0000

    add a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(name='',
+    version='0.1',
+    description='',
+    author='Jaymz Campbell',
+    author_email='jaymz@jaymz.eu',
+    url='https://github.com/jaymzcd/',
+    packages=['',],
+)
+
