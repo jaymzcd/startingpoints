7dc70f92d8bc3fe11f7e8b1de288b09aec711c5a
jaymz <jaymz@u-dox.com>
2011-05-16 11:34:15 +0100

    add a script to make my default folders

---
+++ scripts/genFolders.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+BASE=$1;
+
+mkdir $BASE/public/;
+mkdir $BASE/public/media/;
+mkdir $BASE/public/media/css;
+mkdir $BASE/public/media/js;
+mkdir $BASE/public/media/uploads;
+mkdir $BASE/public/media/img;
+mkdir $BASE/assets;
+mkdir $BASE/assets/sql;
+mkdir $BASE/assets/flat;
+
+
+
+
+
+
