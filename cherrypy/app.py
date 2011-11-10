ee821941aafa3c38184fb2635e055544a6bd7057
jaymz <jaymz@u-dox.com>
2011-11-10 13:15:26 +0000

    added a cherrypy 3 basic app

---
+++ cherrypy/app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+
+import urllib
+import cherrypy
+import re
+import sqlite3
+import os
+import json
+import sys
+
+from jinja2 import Template, Environment, FileSystemLoader
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(PROJECT_PATH)
+DBPATH = os.path.join(PROJECT_PATH, 'entries.db')
+
+env = Environment(loader=FileSystemLoader(os.path.join(PROJECT_PATH, 'templates_flat')))
+
+class CherryStart(object):
+
+    _cp_config = {'tools.sessions.on': True}
+
+    def __init__(self):
+        pass
+
+    def index(self, lang=None, country=None, sku=None):
+        tmpl = env.get_template('content_grid.html')
+        return tmpl.render(context)
+    index.exposed = True
+
+
+STATICDIR = PROJECT_PATH
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+
+conf = {
+    '/': {
+        'tools.encode.on': True,
+        'tools.gzip.on': True,
+    },
+    '/media': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': 'media',
+        'tools.staticdir.root': STATICDIR,
+    },
+}
+
+if __name__=='__main__':
+    cherrypy.server.socket_host = '127.0.0.1'
+    cherrypy.quickstart(CherryStart(), config=conf)
+
+application = cherrypy.Application(CherryStart(), script_name=None, config=conf)
