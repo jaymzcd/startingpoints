f350850216779c489102ec2b9494c0afa2a1a821
jaymz <jaymz@u-dox.com>
2011-11-12 12:17:08 +0000

    some cherrypy start template stuff

--- cherrypy/setup.sh
+++ cherrypy/setup.sh
@@ -5,8 +5,12 @@ mkdir $1/media/css;
 mkdir $1/media/js;
 mkdir $1/templates;
 
+touch $1/media/css/styles.css;
+touch $1/media/js/script.js;
+
 cp cherrypy/app.py $1;
-cp html/html5ish.html $1/templates/master.html;
+cp html/master.html $1/templates/master.html;
+cp html/cherrypy_index.html $1/templates/index.html;
 
 cd $1;
 echo "Go code!";
