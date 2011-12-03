01e550cdf24b3174f3c6c87f3988291c440aa50d
jaymz <jaymz@u-dox.com>
2011-12-03 01:09:27 +0000

    create empty gitignores for folders to force

--- cherrypy/setup.sh
+++ cherrypy/setup.sh
@@ -5,11 +5,14 @@ mkdir $1/media/css;
 mkdir $1/media/js;
 mkdir $1/templates;
 mkdir $1/assets;
+mkdir $1/media/uploads;
 mkdir $1/session_data;
 touch $1/README.md
 
 touch $1/media/css/styles.css;
 touch $1/media/js/script.js;
+touch $1/session_data/.gitignore;
+touch $1/media/uploads/.gitignore;
 
 cp cherrypy/app.py $1;
 cp html/master.html $1/templates/master.html;
