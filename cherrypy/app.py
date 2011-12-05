945a08da461dee64ded4188e33431bc6bc38102d
jaymz <jaymz@u-dox.com>
2011-12-05 12:29:49 +0000

    add profiling via repoze.profile

--- cherrypy/app.py
+++ cherrypy/app.py
@@ -25,7 +25,7 @@ except ImportError:
 env = Environment(loader=FileSystemLoader(os.path.join(PROJECT_PATH, 'templates')))
 env.globals['url'] = cherrypy.url
 
-class CherryStart(object):
+class Root(object):
 
     _cp_config = {'tools.sessions.on': True,
         'tools.sessions.timeout': 60*24,
@@ -55,8 +55,37 @@ conf = {
     },
 }
 
-if __name__=='__main__':
-    cherrypy.server.socket_host = '0.0.0.0'
-    cherrypy.quickstart(CherryStart(), config=conf)
+# for mod_wsgi
+application = cherrypy.Application(Root(), script_name=None, config=conf)
 
-application = cherrypy.Application(CherryStart(), script_name=None, config=conf)
+if __name__=='__main__':
+
+    wsgi_debug = bool(os.environ.get('WSGI_DEBUG', False))
+    if wsgi_debug:
+        # Profile this application by setting an environment variable "WSGI_DEBUG" to 1
+        # See: http://docs.repoze.org/profile/ for the full docs and setup
+        # Load profiling
+        from repoze.profile.profiler import AccumulatingProfileMiddleware
+        middleware = AccumulatingProfileMiddleware(
+            application,
+            log_filename='/tmp/app_profile.log',
+            discard_first_request=True,
+            flush_at_shutdown=True,
+        )
+
+        # There is a wsgi.pipeline conf that takes a WSGI application and applies
+        # middleware to it. Not entirely sure exactly on how to hook this up to
+        # the internal quickstart/cp server but wsgiref will do the same job
+        # and works without much hassle (and is stdlib):
+
+        # See (http://goo.gl/gTKKa) for example (although that's 2008, so guessing)
+        # the pipeline way works as well, just sure how with quickstart
+
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 8080, middleware)
+        print "Serving on 0.0.0.0:8080..."
+        httpd.serve_forever()
+
+    else:
+        cherrypy.server.socket_host = '0.0.0.0'
+        cherrypy.quickstart(Root(), config=conf)
